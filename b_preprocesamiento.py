8aab699ee844851db97a5da85af90b580bdf9e16
juancamiloespana <camilo1260@gmail.com>
2023-02-01 16:02:59 -0500

    cambio de ubicación e info de BD

--- b_preprocesamiento.py
+++ b_preprocesamiento.py
@@ -184,3 +184,12 @@ df.describe(include='all')
 
 
 
+
+
+
+import plotly.express as px
+
+wide_df = px.data.medals_wide()
+
+fig = px.bar(wide_df, x="nation", y=["gold", "silver", "bronze"], title="Wide-Form Input")
+fig.show()
